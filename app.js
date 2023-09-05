7f8cbc05749eccfd6e0541ac54c8c1acbea9c9b7
DaniDhax <DaniDhax@users.noreply.github.com>
2023-09-05 13:06:21 +0200

    Design 4

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const $btnEmail = document.getElementById("btn-email");
 const $errorMessage = document.getElementById("error-message");
 const $section2 = document.querySelector(".section-2");
 
-
 function comprobarEmail() {
   let regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   if (!regex.test($txtEmail.value) || $txtEmail.value === "") {
-    $errorMessage.style.visibility = "visible"
-    $txtEmail.style.border = "2px solid var(--light-red)"
-    $section2.style.rowGap = "35px"
+    $errorMessage.style.visibility = "visible";
+    $txtEmail.style.border = "2px solid var(--light-red)";
+    $section2.style.rowGap = "35px";
+    $txtEmail.value === ""
+      ? ($errorMessage.innerText =
+          "Whoops! It looks like you forgot to add your email")
+      : ($errorMessage.innerText = "Please provide a valid email address");
   } else {
-    $errorMessage.style.visibility = "hidden"
-    $txtEmail.style.border = "1px solid var(--pale-blue)"
-    $section2.style.rowGap = "10px"
+    $errorMessage.style.visibility = "hidden";
+    $txtEmail.style.border = "1px solid var(--pale-blue)";
+    $section2.style.rowGap = "10px";
   }
 }
 
