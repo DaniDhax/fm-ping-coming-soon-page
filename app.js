fac24fa6da07b5f90f09caf592cb50d1f51d3837
DaniDhax <DaniDhax@users.noreply.github.com>
2023-09-05 13:16:23 +0200

    Design 5

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const $txtEmail = document.getElementById("txt-email");
 const $btnEmail = document.getElementById("btn-email");
 const $errorMessage = document.getElementById("error-message");
 const $section2 = document.querySelector(".section-2");
+const $customAlert = document.getElementById("custom-alert");
 
 function comprobarEmail() {
   let regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
@@ -13,11 +14,17 @@ function comprobarEmail() {
       ? ($errorMessage.innerText =
           "Whoops! It looks like you forgot to add your email")
       : ($errorMessage.innerText = "Please provide a valid email address");
+      $customAlert.style.display = "none";
   } else {
     $errorMessage.style.visibility = "hidden";
     $txtEmail.style.border = "1px solid var(--pale-blue)";
     $section2.style.rowGap = "10px";
+    $customAlert.style.display = "block";
   }
 }
 
+function closeCustomAlert() {
+    $customAlert.style.display = "none";
+  }
+  
 $btnEmail.addEventListener("click", comprobarEmail);
